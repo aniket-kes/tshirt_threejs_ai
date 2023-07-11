2348bf651ede52f04ba327f451945ac5ace77cee
Aniket Kesarwani <aniket.kesarwani@spit.ac.in>
2023-07-12 01:23:03 +0530

    changed title

--- client/src/pages/Customizer.jsx
+++ client/src/pages/Customizer.jsx
@@ -47,7 +47,7 @@ const Customizer = () => {
         try {
             setGeneratingImg(true);
 
-            const response = await fetch('http://localhost:8080/api/v1/dalle',{
+            const response = await fetch('https://project-threejs-ai-veid.onrender.com/api/v1/dalle',{
                 method: 'POST',
                 headers: { 'Content-Type': 'application/json'},
                 body: JSON.stringify({
